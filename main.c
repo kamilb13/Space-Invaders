70ecbfc0aeb68eae6a34addb73e2fcdf08a99aa1
Kamil <100875852+kamilb13@users.noreply.github.com>
2023-04-18 21:37:18 +0200

    Update main.c

--- main.c
+++ main.c
@@ -8,15 +8,14 @@
 
 #define SCREEN_WIDTH 1280   
 #define SCREEN_HEIGHT 780   
-#define BALL_SPEED 0.05     
-#define PADDLE_SPEED 0.5
-#define BULLET_SIZE 10
+#define BULLET_SIZE 10     
 
 typedef struct Enemy {
     float x;
     float y;
-    float hp;   
+    float hp;
     float dmg;
+    float speed;
 } Enemy;
 
 typedef struct Ship {
@@ -36,228 +35,220 @@ typedef struct Bullet {
     bool bullet_active;
 } Bullet;
 
-int main(){
+int main() {
+    // INICJACJA GŁÓWNYCH ELEMENTÓW ALLEGRO 5
     al_init();
     al_init_primitives_addon();
     al_install_keyboard();
     al_init_image_addon();
-    al_install_mouse();
-    Ship ship;
-    Bullet bullet;
 
-    bullet.x = SCREEN_WIDTH / 2.0;
-    bullet.y = SCREEN_HEIGHT / 2.0;
-    bullet.vx = BALL_SPEED;
-    bullet.vy = BALL_SPEED;
-    ship.x = 0;
+    
+    // DEKLARACJA I INICJACJA WARTOŚCI STATKU KOSMICZNEGO KONTROLOWANEGO PRZEZ GRACZA
+    Ship ship;
+    ship.x = SCREEN_WIDTH / 2.0;
     ship.y = SCREEN_HEIGHT - 100;
-    bullet.x = 0;
-    bullet.y = 0;
+    ship.vx = 0.5;
+
+
+    // DEKLARACJA I INICJACJA WARTOŚCI POCISKU WYSTRZELIWANEGO PRZEZ GRACZA
+    Bullet bullet;
+    bullet.x = -10;
+    bullet.y = -10;
     bullet.vx = 0;
     bullet.vy = -1;
     bullet.bullet_active = false;
 
 
+    // DYNAMICZNA ALOKACJA TABLICY JAKO KONTENER NA PRZECIWNIKÓW
+    Enemy* opponents = (Enemy*)calloc(5, sizeof(Enemy));  
 
-    Enemy enemy;
-    enemy.x = 150;
-    enemy.y = 300;
-    enemy.dmg = 1;
-    enemy.hp = 2;
-
-    
-    Enemy* opponents = (Enemy*)calloc(5, sizeof(Enemy));   //Tablica z przeciwnikami
-    Enemy* enemy_p = (Enemy*)malloc(sizeof(Enemy));
-    Enemy* null_enemy = NULL;
+    // INICJACJA WARTOŚCI KAŻDEGO Z PRZECIWNIKÓW
     opponents[0].x = 450;
     opponents[0].y = 300;
     opponents[0].dmg = 1;
     opponents[0].hp = 2;
+    opponents[0].speed = 0.04;
 
     opponents[1].x = 600;
     opponents[1].y = 400;
     opponents[1].dmg = 1;
     opponents[1].hp = 2;
+    opponents[1].speed = 0.04;
 
     opponents[2].x = 150;
     opponents[2].y = 300;
     opponents[2].dmg = 1;
     opponents[2].hp = 7;
+    opponents[2].speed = 0.04;
 
     opponents[3].x = 23;
     opponents[3].y = 429;
     opponents[3].dmg = 1;
     opponents[3].hp = 2;
+    opponents[3].speed = 0.04;
 
     opponents[4].x = 500;
     opponents[4].y = 100;
     opponents[4].dmg = 1;
     opponents[4].hp = 2;
+    opponents[4].speed = 0.04;
 
-	enemy_p->x = 450;
-	enemy_p->y = 300;
-	enemy_p->dmg = 1;
-	enemy_p->hp = 2;
+ 
 
-    //float ball_x = SCREEN_WIDTH / 2.0;
-    //float ball_y = SCREEN_HEIGHT / 2.0;
-    //float ball_vx = BALL_SPEED;
-    //float ball_vy = BALL_SPEED;
-    //float paddle_x = 0;
-    //float paddle_y = SCREEN_HEIGHT - 100;
-    //float bullet_x = 0;
-    //float bullet_y = 0;
-    //float bullet_vx = 0;
-    //float bullet_vy = -1;
-    //bool bullet_active = false;
-
-
-
-    //Tworzę zmienną display typu ALLEGRO_DISPLAY
+    // DEKLARACJA I INICJACJA OKNA GRY
     ALLEGRO_DISPLAY* display = al_create_display(SCREEN_WIDTH, SCREEN_HEIGHT);
     al_set_window_title(display, "Space Invaders");
 
+    // DEKLARACJA I INICJACJA KOLORÓW UŻYWANYCH DO RYSOWANIA OBIEKTÓW
     ALLEGRO_COLOR bg_color = al_map_rgb(0, 0, 0);
-    ALLEGRO_COLOR ball_color = al_map_rgb(255, 255, 255);
-    ALLEGRO_COLOR paddle_color = al_map_rgb(255, 255, 255);
     ALLEGRO_COLOR bullet_color = al_map_rgb(0, 255, 0);
-    ALLEGRO_COLOR object_color = al_map_rgb(255, 255, 255);
 
+    // DEKLARACJA 
     ALLEGRO_KEYBOARD_STATE keyboard_state;
-    ALLEGRO_MOUSE_STATE mouse_state;
+	ALLEGRO_FONT* font = al_create_builtin_font();
 
-    ALLEGRO_BITMAP* bitmap = al_load_bitmap("C:/Users/kamil/Desktop/player.png");
-    ALLEGRO_BITMAP* enemy_bitmap = al_load_bitmap("C:/Users/kamil/Desktop/ufo.png");
-    if (!bitmap){
-        printf("Nie udalo sie zaladowac obrazka!\n");
+    // ŁADOWANIE GRAFIK REPREZENTUJĄCYCH STATKI KOSMICZNE
+    ALLEGRO_BITMAP* bitmap = al_load_bitmap("C:/Users/KOMPUTER/Desktop/ALLEGRO GRY/PNGsy/space-ship_3.png");
+    ALLEGRO_BITMAP* enemy_bitmap = al_load_bitmap("C:/Users/KOMPUTER/Desktop/ALLEGRO GRY/PNGsy/ufo.png");
+
+
+    // DEKLARACJA I INICJACJA "STOPERA"
+    ALLEGRO_TIMER* timer = NULL;
+    timer = al_create_timer(0.001); // 
+
+    // SPRAWDZENIE POPRAWNOSCI ZMIENNYCH
+    if (!timer) {
+        printf("Nie udalo zainicjowac timera\n");
+        return -1;
+    }
+    
+    if (!bitmap) {
+        printf("Nie udalo sie zaladowac obrazka statku kosmicznego gracza!\n");
         return -1;
     }
 
-	int enemy_iter; // ITERATOR TABLICY WROGOW
+    if (!enemy_bitmap) {
+        printf("Nie udalo sie zaladowac obrazka statku kosmicznego przeciwnikow!\n");
+        return -1;
+    }
 
-    //Ekran startowy gry 
-    int kiszka = 1;
-    while (kiszka) {  // prostokat, jeżeli (MyszkaX > ButtonX) Oraz (MyszkaY > ButtonY) Oraz (MyszkaX < ButtonX+ButtonWidth) Oraz (MyszkaY < ButtonY+ButtonHeight) Oraz (LewyPrzyciskMyszkiNacisniety) wtedy ...
+    // DEKLARACJA I INICJACJA DODATKOWYCH ZMIENNYCH
+    int enemy_iter;                 // Iterator tablicy wrogów
+    float enemy_move = 0;           // Zmienna pomocnicza wykorzystywana do ruchu przeciwników
+
+
+    // Ekran startowy gry
+    int start_window = 1;
+    while (start_window) {  // prostokat, jeżeli (MyszkaX > ButtonX) Oraz (MyszkaY > ButtonY) Oraz (MyszkaX < ButtonX+ButtonWidth) Oraz (MyszkaY < ButtonY+ButtonHeight) Oraz (LewyPrzyciskMyszkiNacisniety) wtedy ...
         al_get_keyboard_state(&keyboard_state);
         al_clear_to_color(bg_color); //ustawia tlo
+        al_flip_display();
         al_draw_filled_rectangle(SCREEN_WIDTH / 2 - 300, SCREEN_HEIGHT / 2 - 50, SCREEN_WIDTH / 2 + 300, SCREEN_HEIGHT / 2 + 100, al_map_rgb(220, 200, 220));
         al_draw_text(font, al_map_rgb(0, 0, 0), SCREEN_WIDTH / 2 - 40, SCREEN_HEIGHT / 2 + 30, ALLEGRO_ALIGN_CENTER, "Nacisnij ENTER by rozpoczac gre...");
         if (al_key_down(&keyboard_state, ALLEGRO_KEY_ENTER)) {
-            kiszka = 0;
+            start_window = 0;
         }
 
         al_flip_display();  //odswieza ekran
     }
-	
-    while (1){
 
-        al_clear_to_color(bg_color);
 
-        // Przetwarzanie zdarzeń klawiatury i myszy
-        al_get_keyboard_state(&keyboard_state);
-        //al_get_mouse_state(&mouse_state);
-        if (al_key_down(&keyboard_state, ALLEGRO_KEY_LEFT)) {
-            //paddle_x -= PADDLE_SPEED;
-            ship.x -= PADDLE_SPEED;
-            if (ship.x < 0) {
-                ship.x = 0;
+    // WYSTARTOWANIE TIMERA
+    al_start_timer(timer);
+
+
+    // GŁÓWNA PĘTLA GRY
+    do {
+        // SPRAWDZENIE 
+        if (al_get_timer_count(timer) > 0){
+            al_set_timer_count(timer, 0);
+            al_clear_to_color(bg_color);
+
+            // Przetwarzanie zdarzeń klawiatury i myszy
+            al_get_keyboard_state(&keyboard_state);
+            if (al_key_down(&keyboard_state, ALLEGRO_KEY_LEFT)) {
+                ship.x -= ship.vx;
+                if (ship.x < 0) {
+                    ship.x = 0;
+                }
             }
-        }
 
-        if (al_key_down(&keyboard_state, ALLEGRO_KEY_RIGHT)){
-            //al_clear_to_color(bg_color);
-            ship.x += PADDLE_SPEED;
-            if (ship.x > SCREEN_WIDTH - 150) {
-                al_clear_to_color(bg_color);
-                ship.x = SCREEN_WIDTH - 150;
+            if (al_key_down(&keyboard_state, ALLEGRO_KEY_RIGHT)) {
+                ship.x += ship.vx;
+                if (ship.x > SCREEN_WIDTH - 150) {
+                    al_clear_to_color(bg_color);
+                    ship.x = SCREEN_WIDTH - 150;
+                }
             }
-        }
-        if (al_key_down(&keyboard_state, ALLEGRO_KEY_SPACE) && !bullet.bullet_active){
-            puts("klikam spacje ");
-            bullet.bullet_active = true;
-            bullet.x = ship.x + 70;
-            bullet.y = ship.y;
-
-        }
-		
-
-        // Rysowanie pocisku
-        if (bullet.bullet_active) {
-
-            al_draw_filled_rectangle(bullet.x, bullet.y, bullet.x + BULLET_SIZE, bullet.y + BULLET_SIZE, bullet_color);
-            if (bullet.y == SCREEN_HEIGHT) {
-                printf("kiszka");
+            if (al_key_down(&keyboard_state, ALLEGRO_KEY_SPACE) && !bullet.bullet_active) {
+                puts("klikam spacje ");
+                bullet.bullet_active = true;
+                bullet.x = ship.x + 70;
+                bullet.y = ship.y;
             }
-            // Aktualizacja pozycji pocisku
-            bullet.bullet_active = true;
+
+
+            // Rysowanie pocisku
             if (bullet.bullet_active) {
 
-                bullet.y += bullet.vy;
-
-
-            }
-        }
-
-
-        // Sprawdzenie, czy pocisk wyleciał poza ekran
-        if (bullet.y < 0) {
-            bullet.bullet_active = false;
-        } 
-
-        if (bullet.x > enemy.x && bullet.x < enemy.x + 80 && bullet.y < enemy.y + 20) {
-            enemy.hp -= 1;
-            bullet.bullet_active = false;
-            bullet.x = -1;
-            bullet.y = -1;
-        }
-
-		if (bullet.x > enemy_p->x && bullet.x < enemy_p->x + 80 && bullet.y < enemy_p->y + 20) { // CUdenko dynamiczne
-            free(enemy_p);
-            //enemy_p = NULL;
-			bullet.bullet_active = false;
-            bullet.x = -10;
-            bullet.y = -10;
-		}
-
-        if (enemy.hp > 0) {     // słabe, do wyrzucenia 
-            al_draw_bitmap(enemy_bitmap, enemy.x, enemy.y, 0);
-        }
-        else {
-            enemy.x = -10;
-            enemy.y = -10;
-        }
-            
-
-        if(enemy_p) {
-			al_draw_bitmap(enemy_bitmap, enemy_p->x, enemy_p->y, 0);
-        }
-
-
-        for (enemy_iter = 0; enemy_iter < 5; enemy_iter++) {
-            if (bullet.x > opponents[enemy_iter].x && bullet.x < opponents[enemy_iter].x + 80 && bullet.y < opponents[enemy_iter].y + 20) {
-                opponents[enemy_iter].hp -= 1;
-                //lista  z przeciwnikami 
-                if (!opponents[enemy_iter].hp) {
-                    opponents[enemy_iter].x = -100;
-                    opponents[enemy_iter].y = -100;
+                al_draw_filled_rectangle(bullet.x, bullet.y, bullet.x + BULLET_SIZE, bullet.y + BULLET_SIZE, bullet_color);
+                
+                // Aktualizacja pozycji pocisku
+                bullet.bullet_active = true;
+                if (bullet.bullet_active) {
+                    bullet.y += bullet.vy;
                 }
-                bullet.bullet_active = false;
-                bullet.x = -10;
-                bullet.y = -10;
             }
+
+
+            // Sprawdzenie, czy pocisk wyleciał poza ekran
+            if (bullet.y < 0) {
+                bullet.bullet_active = false;
+            }
+
+
+
+            for (enemy_iter = 0; enemy_iter < 5; enemy_iter++) {
+                if (bullet.x > opponents[enemy_iter].x && bullet.x < opponents[enemy_iter].x + 80 && bullet.y < opponents[enemy_iter].y + 20) {
+                    opponents[enemy_iter].hp -= 1;
+                    //lista  z przeciwnikami 
+                    if (!opponents[enemy_iter].hp) {
+                        opponents[enemy_iter].x = -100;
+                        opponents[enemy_iter].y = -100;
+                    }
+
+                    bullet.bullet_active = false;
+                    bullet.x = -10;
+                    bullet.y = -10;
+                }
+            }
+
+            if (enemy_move > 100 || enemy_move < -100) {
+                for (enemy_iter = 0; enemy_iter < 5; enemy_iter++) {
+                    opponents[enemy_iter].speed *= -1;
+                }
+            }
+
+            for (enemy_iter = 0; enemy_iter < 5; enemy_iter++) {
+                opponents[enemy_iter].x += opponents[enemy_iter].speed;
+                if (opponents[enemy_iter].hp) {
+                    al_draw_bitmap(enemy_bitmap, opponents[enemy_iter].x, opponents[enemy_iter].y, 0);
+                }
+            }
+
+            enemy_move += opponents[0].speed;
+            enemy_move += opponents[1].speed;
+            enemy_move += opponents[2].speed;
+            enemy_move += opponents[3].speed;
+            enemy_move += opponents[4].speed;
+
+
+            al_draw_bitmap(bitmap, ship.x, ship.y, 0);
+            al_flip_display();
         }
-        
-        for (enemy_iter = 0; enemy_iter < 5 ; enemy_iter++) {
-			if (opponents[enemy_iter].hp) {
-				al_draw_bitmap(enemy_bitmap, opponents[enemy_iter].x, opponents[enemy_iter].y, 0);
-			}
-        }
-        
-        
-        al_draw_bitmap(bitmap, ship.x, ship.y, 0);
-        al_flip_display();
-    }
-    
+    } while (1);
+
+
     al_destroy_bitmap(bitmap);
     al_destroy_display(display);
     free(opponents);
